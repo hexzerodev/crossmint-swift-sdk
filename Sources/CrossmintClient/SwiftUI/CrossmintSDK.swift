efb4f12d1e673d999fb3adb520fcd67ce985b5fe
Brendan <brendan@basebeta.com>
2026-01-19 14:36:41 -0700

    Fix: isOTPRequred -> isOTPRequired (#7)
    * fix typo isOTPRequred -> isOTPRequired
    
    * Fix isOTPRequired typo in SolanaDemo
    
    * Fix build error post mergeback
    
    ---------
    
    Co-authored-by: Tomás Martins <tomas.temp@paella.dev>

--- Sources/CrossmintClient/SwiftUI/CrossmintSDK.swift
+++ Sources/CrossmintClient/SwiftUI/CrossmintSDK.swift
@@ -46,7 +46,7 @@ final public class CrossmintSDK: ObservableObject {
 
     let crossmintTEE: CrossmintTEE
 
-    public var isOTPRequred: Published<Bool>.Publisher {
+    public var isOTPRequired: Published<Bool>.Publisher {
         crossmintTEE.$isOTPRequired
     }
     public func submit(otp: String) {
